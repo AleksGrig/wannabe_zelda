2f7ba107526ba0c399a26a4ec316dc58390e9f1f
AleksGrig <ullitap@gmail.com>
2025-09-18 12:22:21 +0300

    episode 6: complex animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,9 +3,11 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var last_lean := 0.0
+
 @onready var camera: Node3D = $CameraRig/Camera3D
 @onready var animation_player: AnimationPlayer = $Mesh/AnimationPlayer
-
+@onready var anim_tree: AnimationTree = $AnimationTree
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -35,12 +37,20 @@ func _physics_process(delta: float) -> void:
 	const RUN_SPEED := 3.5
 	const BLEND_SPEED := 0.2
 	
-	if current_speed > RUN_SPEED:
-		animation_player.play("freehand_run", BLEND_SPEED)
+	if not is_on_floor():
+		anim_tree.set("parameters/movement/transition_request", "fall")
+	elif current_speed > RUN_SPEED:
+		#print(global_basis.x)
+		anim_tree.set("parameters/movement/transition_request", "run")
+		var lean := direction.dot(global_basis.x)
+		last_lean = lerpf(last_lean, lean, 0.4)
+		anim_tree.set("parameters/run_lean/add_amount", lean)
 	elif current_speed > 0:
-		animation_player.play("freehand_walk", BLEND_SPEED, lerp(0.5, 1.75, current_speed/RUN_SPEED))
+		var walk_speed := lerpf(0.5, 1.75, current_speed/RUN_SPEED)
+		anim_tree.set("parameters/movement/transition_request", "walk")
 	else:
 		animation_player.play("freehand_idle")
+		anim_tree.set("parameters/movement/transition_request", "idle")
 	
 
 func turn_to(direction: Vector3) -> void:
